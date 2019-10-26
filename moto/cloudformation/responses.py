211a1c710e5a734cb89180a2cd1ccf82dd9e61ea
Bhavesh Nigam <bhavesh.nigam@yahoo.com>
2019-10-26 10:13:45 +0400

    .

--- moto/cloudformation/responses.py
+++ moto/cloudformation/responses.py
@@ -207,7 +207,7 @@ class CloudFormationResponse(BaseResponse):
 
     def describe_stack_events(self):
         stack_name = self._get_param('StackName')
-        stack_name = stack_name.split(':')[-2]
+        stack_name = stack_name.split(":")[-1].split("/")[1]
         stack = self.cloudformation_backend.get_stack(stack_name)
 
         template = self.response_template(DESCRIBE_STACK_EVENTS_RESPONSE)
